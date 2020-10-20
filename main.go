1df6989185a796988b250869223f0a44d5a715fe
Kevin Wyckmans <kevinwyckmans@gmail.com>
2020-10-20 22:12:09 +0200

    load existing properties and don't parse known ones.

--- main.go
+++ main.go
@@ -15,15 +15,86 @@ import (
 )
 
 func main() {
-	const root = "https://www.era.be"
 	const startPrice = 200000
 	const maxPrice = 400000
 
-	properties := make([]Property, 1)
+	log.Println("Load properties")
+	var properties []Property = loadProperties("test.json")
 
-	c := colly.NewCollector(
-	// colly.AllowedDomains("era.be"),
-	)
+	log.Println("Start scraping")
+	properties = scrapeEra(properties, startPrice, maxPrice)
+
+	log.Println("Finished scraping, saving to file")
+
+	// This will dump everything in proprties to a file. Saving everything everytime
+	// seems awefully inefficient, but it will have to do for now.
+	// An easy optimisation would be to check if I even found any updates, but that will
+	// happen very often, so only has a minor impact.
+	saveResults(properties, "test.json")
+}
+
+/**
+Checks if we already found any given property based on the url where we found it.
+This works for now, but will not work once a second website for scraping is added.
+
+This is also the reason I'm not using maps to store the properties in for now. I could
+store properties in a map, using the url as key, but that again would break down once a second
+site is added that may contain the same properties (and I know for a fact that that will happen).
+*/
+func findProperty(properties []Property, url url.URL) int {
+	for i, p := range properties {
+		if url == p.URL {
+			return i
+		}
+	}
+
+	return -1
+}
+
+func parseProperty(e *colly.HTMLElement) Property {
+	title := e.ChildText("h1")
+	address := e.ChildText(".field-name-era-adres--c a[href]")
+	var maps, err = url.Parse(e.ChildAttr(".field-name-era-adres--c a[href]", "href"))
+
+	var bedrooms int
+	var bathrooms int
+	var livingArea int
+	var price int
+
+	rawBathrooms := e.ChildText(".field-name-era-aantal-badkamers--c .era-tooltip-field")
+	if len(rawBathrooms) > 0 {
+		bathrooms, err = strconv.Atoi(rawBathrooms)
+	}
+
+	rawBedrooms := e.ChildText(".field-name-era-aantal-slaapkamers--c .era-tooltip-field")
+	if len(rawBedrooms) > 0 {
+		bedrooms, err = strconv.Atoi(rawBedrooms)
+	}
+
+	rawLivingArea := strings.TrimSpace(strings.Split(e.ChildText(".field-name-era-oppervlakte-bewoonbaar--c .era-tooltip-field"), "m")[0])
+	if len(rawLivingArea) > 0 {
+		livingArea, err = strconv.Atoi(rawLivingArea)
+	}
+
+	rawPrice := e.ChildText(".field-name-era-actuele-vraagprijs--c .field-item")
+	if len(rawPrice) > 0 && rawPrice != "Op aanvraag" {
+		rawPrice = strings.Replace(strings.Split(rawPrice, "€")[1], " ", "", -1)
+		price, err = strconv.Atoi(rawPrice)
+	} else {
+		price = math.MaxInt64
+	}
+
+	if err != nil {
+		log.Println("an error occured!", err)
+	}
+
+	return NewProperty(price, livingArea, bedrooms, bathrooms, address, *maps, title, *e.Request.URL)
+}
+
+func scrapeEra(properties []Property, startPrice int, maxPrice int) []Property {
+	const root = "https://www.era.be"
+
+	c := colly.NewCollector()
 
 	c.Limit(&colly.LimitRule{
 		DomainGlob:  "*era.*",
@@ -33,69 +104,21 @@ func main() {
 
 	c.OnHTML(".era-search--result-nodes .node-property .field-name-node-link a[href]", func(e *colly.HTMLElement) {
 		link := e.Attr("href")
-		// fmt.Println("found:", link)
 		c.Visit(e.Request.AbsoluteURL(link))
 	})
 
 	c.OnHTML(".pager li .last", func(e *colly.HTMLElement) {
-		// fmt.Println(e.Attr("href"))
 		nextPage := e.Attr("href")
 		c.Visit(e.Request.AbsoluteURL(nextPage))
 	})
 
 	c.OnHTML(".intro", func(e *colly.HTMLElement) {
-		fmt.Println(e.Request.URL)
-		title := e.ChildText("h1")
-		address := e.ChildText(".field-name-era-adres--c a[href]")
-		var maps, err = url.Parse(e.ChildAttr(".field-name-era-adres--c a[href]", "href"))
-
-		var bedrooms int
-		var bathrooms int
-		var livingArea int
-		var price int
-
-		rawBathrooms := e.ChildText(".field-name-era-aantal-badkamers--c .era-tooltip-field")
-		if len(rawBathrooms) > 0 {
-			bathrooms, err = strconv.Atoi(rawBathrooms)
-		}
-
-		rawBedrooms := e.ChildText(".field-name-era-aantal-slaapkamers--c .era-tooltip-field")
-		if len(rawBedrooms) > 0 {
-			bedrooms, err = strconv.Atoi(rawBedrooms)
-		}
-
-		rawLivingArea := strings.TrimSpace(strings.Split(e.ChildText(".field-name-era-oppervlakte-bewoonbaar--c .era-tooltip-field"), "m")[0])
-		if len(rawLivingArea) > 0 {
-			livingArea, err = strconv.Atoi(rawLivingArea)
-		}
-
-		rawPrice := e.ChildText(".field-name-era-actuele-vraagprijs--c .field-item")
-		if len(rawPrice) > 0 && rawPrice != "Op aanvraag" {
-			rawPrice = strings.Replace(strings.Split(rawPrice, "€")[1], " ", "", -1)
-			price, err = strconv.Atoi(rawPrice)
+		if findProperty(properties, *e.Request.URL) == -1 {
+			property := parseProperty(e)
+			properties = append(properties, property)
 		} else {
-			price = math.MaxInt64
+			log.Println("Encountered property that was already known")
 		}
-
-		if err != nil {
-			log.Println("an error occured!", err)
-			// panic(err)
-		}
-
-		property := NewProperty(price, livingArea, bedrooms, bathrooms, address, *maps, title, *e.Request.URL)
-		properties = append(properties, property)
-		// file, _ := json.MarshalIndent(property, "", " ")
-		// fmt.Println(file)
-		// _ = ioutil.WriteFile("test.json", file, 0644)
-		// fmt.Printf("%+v", property)
-		// fmt.Println(title, address, maps, bathrooms, bedrooms, livingArea, price)
-
-	})
-
-	// Find and visit all links
-
-	c.OnRequest(func(r *colly.Request) {
-		// fmt.Println("Visiting", r.URL)
 	})
 
 	c.OnError(func(r *colly.Response, e error) {
@@ -103,15 +126,26 @@ func main() {
 	})
 
 	var url = fmt.Sprintf("%s/nl/te-koop/wilrijk?broker_id=52636&price=%d+%d", root, startPrice, maxPrice)
-	// var url = "https://www.era.be/nl/te-koop/wilrijk/appartement/luxe-appartement-3slpk-dubbele-garagebox-en-staanplaats?broker_id=52636"
+
 	c.Visit(url)
 
-	log.Println("Finished scraping")
+	return properties
+}
 
+func loadProperties(filename string) []Property {
+	data, err := ioutil.ReadFile(filename)
+
+	if err != nil {
+		panic(err)
+	}
+
+	var properties []Property
+	err = json.Unmarshal(data, &properties)
+
+	return properties
+}
+
+func saveResults(properties []Property, filename string) {
 	file, _ := json.MarshalIndent(properties, "", " ")
-	_ = ioutil.WriteFile("test.json", file, 0644)
-
-	// for _, property := range properties {
-	// 	fmt.Printf("%+v\n", property)
-	// }
+	_ = ioutil.WriteFile(filename, file, 0644)
 }
